2d619e29d3bc20224cc796569c4dab2333397c29
justintucker <switch.one@gmail.com>
2023-02-19 13:37:57 -0700

    Add blog post collection

---
+++ src/content/blog/post-1.md
@@ -0,0 +1,8 @@
+---
+title: "Post Title 1"
+tags: ["running", "jogging", "swimming"]
+---
+
+# Post 1
+
+## Some Title
--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,15 @@ const articleCollection = defineCollection({
   }),
 });
 
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
+  }),
+});
+
 export const collections = {
   articles: articleCollection,
+  posts: blogCollection,
 };
